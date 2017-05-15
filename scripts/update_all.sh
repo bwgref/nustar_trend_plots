cd240cb01b10fbd5bc2df9a4e28f7ca269cc28b0
Brian Greftenstette <bwgref@lif.(none)>
2017-05-15 13:21:13 -0700

    Adding the minmax temps to the cron job proc.

--- scripts/update_all.sh
+++ scripts/update_all.sh
@@ -11,6 +11,7 @@ now=`date`
 echo $now >> update.log
 ./update_trends.sh >> update.log 2>&1
 ./update_temperatures.sh >> update.log 2>&1
+./update_temperatures_minmax.sh >> update.log 2>&1
 ./dump_data.sh >> update.log 2>&1
 ./push_slack.sh update.log >> /dev/null
 

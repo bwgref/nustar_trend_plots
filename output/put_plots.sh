cd5aba7f400256777cbc60f91d1027bd8fc98073
Brian Greftenstette <bwgref@lif.(none)>
2017-12-01 15:46:16 -0800

    Updated script to push the plots to the trend plot Web directory.

--- output/put_plots.sh
+++ output/put_plots.sh
@@ -1,2 +1,8 @@
 cp longterm_trends.pdf web_dir/
 chmod 664 web_dir/longterm_trends.pdf 
+
+cp latest_max_temps_vs_saa.pdf web_dir/
+chmod 664 web_dir/latest_max_temps_vs_saa.pdf
+
+cp max_temps_vs_saa.pdf web_dir/
+chmod 664 web_dir/max_temps_vs_saa.pdf

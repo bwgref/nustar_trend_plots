db7e9c478e7f462b7edd8374e8305698c0f9d851
Brian Greftenstette <bwgref@lif.(none)>
2017-12-01 15:49:04 -0800

    Fixed duplicated of dump_data and cleaned up the output log message.

--- scripts/update_all.sh
+++ scripts/update_all.sh
@@ -8,31 +8,31 @@ export TROOT=/disk/lif2/bwgref/git/nustar_trend_plots
 cd $TROOT/scripts
 
 echo "Updating trend data." > update.log
+echo >> update.log
 now=`date`
 echo $now >> update.log
 echo "Updating trends" >> update.log
 ./update_trends.sh >> update.log 2>&1
+echo >> update.log
 
 now=`date`
 echo $now >> update.log
 echo "Updating mean temperatures" >> update.log 2>&1
 ./update_temperatures.sh >> update.log 2>&1
+echo >> update.log
 
 now=`date`
 echo $now >> update.log
 echo "Updating min/max temperatures" >> upate.log 2>&1
 ./update_temperatures_minmax.sh >> update.log 2>&1
+echo >> update.log
+
 
 now=`date`
 echo $now >> update.log
 echo "Generating output files" >> update.log 2>&1
 ./dump_data.sh >> update.log 2>&1
-
-now=`date`
-echo $now >> update.log
-echo "Generating output files" >> update.log 2>&1
-./dump_data.sh >> update.log 2>&1
-
+echo >> update.log
 
 now=`date`
 echo $now >> update.log
